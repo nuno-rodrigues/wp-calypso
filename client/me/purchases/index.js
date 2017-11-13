b8dfff92984df210225acbdb6df92fc951e345b4
Dennis Snell <dennis.snell@automattic.com>
2017-11-12 22:58:35 -0500

    esmodules: Update my-sites controller for named exports/imports
    @see #18838
    
    This is meant to be a prerparatory part of #18838 or at least a
    part/chunk of that PR in order to make it smaller and easier to
    review and test and evaluate.
    
    Many of our modules were importing the default export value from
    the `my-sites` controller and calling methods off of that default
    
    ```js
    import controller from 'my-sites/controller';
    
    page( '/path', controller.siteNavigation );
    ```
    
    With the controller exporting this large object as its default export
    and with the other modules importing it in its entirety it meant that
    these other modules would depend on importing the entire controller
    module even though they may only need a small fraction of it.
    
    Using named imports and exports gets us closer to the point where we
    can stop transpiling our modules into CommonJS modules before sending
    them to Webpack. When that happens (after this and other PRs merge)
    then Webpack will start "tree-shaking" our build, meaning that it
    will only pull in these statically-defined imports instead of the
    referenced modules in their entirety.

--- client/me/purchases/index.js
+++ client/me/purchases/index.js
@@ -1,9 +1,7 @@
+/** @format */
 /**
  * External Dependencies
- *
- * @format
  */
-
 import config from 'config';
 import page from 'page';
 
@@ -12,7 +10,7 @@ import page from 'page';
  */
 import billingController from 'me/billing-history/controller';
 import meController from 'me/controller';
-import sitesController from 'my-sites/controller';
+import { siteSelection } from 'my-sites/controller';
 import controller from './controller';
 import paths from './paths';
 
@@ -34,7 +32,7 @@ export default function() {
 		paths.managePurchase(),
 		meController.sidebar,
 		controller.noSitesMessage,
-		sitesController.siteSelection,
+		siteSelection,
 		controller.managePurchase
 	);
 
@@ -42,7 +40,7 @@ export default function() {
 		paths.cancelPurchase(),
 		meController.sidebar,
 		controller.noSitesMessage,
-		sitesController.siteSelection,
+		siteSelection,
 		controller.cancelPurchase
 	);
 
@@ -50,7 +48,7 @@ export default function() {
 		paths.cancelPrivacyProtection(),
 		meController.sidebar,
 		controller.noSitesMessage,
-		sitesController.siteSelection,
+		siteSelection,
 		controller.cancelPrivacyProtection
 	);
 
@@ -58,7 +56,7 @@ export default function() {
 		paths.confirmCancelDomain(),
 		meController.sidebar,
 		controller.noSitesMessage,
-		sitesController.siteSelection,
+		siteSelection,
 		controller.confirmCancelDomain
 	);
 
@@ -66,7 +64,7 @@ export default function() {
 		paths.addCardDetails(),
 		meController.sidebar,
 		controller.noSitesMessage,
-		sitesController.siteSelection,
+		siteSelection,
 		controller.addCardDetails
 	);
 
@@ -74,7 +72,7 @@ export default function() {
 		paths.editCardDetails(),
 		meController.sidebar,
 		controller.noSitesMessage,
-		sitesController.siteSelection,
+		siteSelection,
 		controller.editCardDetails
 	);
 
